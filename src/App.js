b91e975d39a4728c129b147865281dce3f97c57c
BillyStudy <billystudy@hotmail.com>
2020-03-02 15:59:58 -0300

    Creating application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellow</h1> 
+    </div>
+  );
+}
+
+export default App;
+
+// yarn create react-app [nome_do_projeto]
