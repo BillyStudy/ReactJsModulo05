0ea9b2ad6a9c094fa3b66338876bce11656c30a1
BillyStudy <billystudy@hotmail.com>
2020-03-09 17:23:09 -0300

    Showing issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 import PropTypes from 'prop-types';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 // import { Container } from './styles';
 
@@ -32,7 +32,7 @@ export default class Repository extends Component {
       api.get(`/repos/${repoName}/issues`, {
         params: {
           state: 'open',
-          per_page: 5
+          per_page: 10
         }
       })
     ]);
@@ -40,6 +40,7 @@ export default class Repository extends Component {
 
     this.setState({
       repository: repository.data,
+      issues: issues.data,
       loading: false
     });
   }
@@ -58,11 +59,33 @@ export default class Repository extends Component {
       <Container>
         <Link to={`/`}>Voltar</Link>
         <Owner>
-          
+
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {
+            issues.map(issue => (
+              <li key={String(issue.id)}>
+                <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                <div>
+                  <strong>
+                    <a href={issue.html_url}>{issue.title}</a>
+                    {issue.labels.map(label => (
+                      <span key={String(label.id)}>{label.name}</span>
+                    ))}
+                  </strong>
+                  <p>{issue.user.login}</p>
+                </div>
+              </li>
+
+            ))
+          }
+
+        </IssueList>
       </Container>
 
     )
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -14,11 +14,12 @@ export const Owner = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    
 
     img{
         width: 120px;
         border-radius: 50%;
-        margin-top: 25px;
+        margin-top: 10px;
     }
 
     h1{
@@ -34,6 +35,85 @@ export const Owner = styled.div`
         text-align: center;
         max-width: 350px;
     }
+`;
+
+export const IssueList = styled.ul`
+    list-style: none;
+    margin-top: 30px;
+    padding-top: 30px;
+    border-top: 0.3px solid #ddd;
+    img{
+        width: 40px;
+        height: 40px;
+        border: 0.3px solid #ccc;
+        border-radius: 50%;
+        
+    }
+
+    div{
+        flex: 1;
+        margin-left: 20px;
+
+        strong{
+            margin-top: 5px;
+            font-size: 14px;
+            color: #444;
+            line-height: 1.4;
+            text-align: center;
+            max-width: 150px;
+
+            a{
+                text-decoration: none;
+                color: #333;
+
+                &:hover{
+                    color: #3152bd;
+                }
+            }
+
+            span{
+                background-color: #eee;
+                color: #333;
+                padding: 2px 6px;
+                border-radius: 5px;
+                font-size: 12px;
+                font-weight: 600;
+                margin: 0px 3px;
+
+                &:hover{
+                    background-color: #ddd;
+                }
+            }
+        }
+
+        p{
+            margin-top: 5px;
+            font-size:12px;
+            color: #aaa;
+        }
+    }
+
+    
+
+    li{
+         padding: 15px 10px;
+         display: flex;
+         flex-direction: row;
+         align-items: center;
+         border: 0.4px solid #aaa;
+         border-radius: 4px;
+
+         & + li {
+             margin-top: 10px;
+         }
+     }
+
+     
+
+     a{
+         color: #3152bd;
+         text-decoration: none;
+     }
 `;
 
 
